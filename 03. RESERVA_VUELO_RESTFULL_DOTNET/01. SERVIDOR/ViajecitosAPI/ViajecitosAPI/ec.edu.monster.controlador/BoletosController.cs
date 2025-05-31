e83a46188f2836a053d9c58c9cc8ba84257f784d
adalicarvajal <lacarvajal4@espe.edu.ec>
2025-05-31 16:08:13 -0500

    Servicio Rest

---
+++ 03. RESERVA_VUELO_RESTFULL_DOTNET/01. SERVIDOR/ViajecitosAPI/ViajecitosAPI/ec.edu.monster.controlador/BoletosController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViajecitosAPI.Data;
+using ViajecitosAPI.ec.edu.monster.modelo;
+namespace ViajecitosAPI.ec.edu.monster.controlador
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BoletosController : ControllerBase
+    {
+        private readonly ViajecitosContext _context;
+
+        public BoletosController(ViajecitosContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Boleto>>> GetBoletos()
+        {
+            return await _context.Boletos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Boleto>> GetBoleto(int id)
+        {
+            var boleto = await _context.Boletos.FindAsync(id);
+            if (boleto == null) return NotFound();
+            return boleto;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Boleto>> PostBoleto(Boleto boleto)
+        {
+            _context.Boletos.Add(boleto);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetBoleto), new { id = boleto.id_boleto }, boleto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBoleto(int id, Boleto boleto)
+        {
+            if (id != boleto.id_boleto) return BadRequest();
+            _context.Entry(boleto).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBoleto(int id)
+        {
+            var boleto = await _context.Boletos.FindAsync(id);
+            if (boleto == null) return NotFound();
+            _context.Boletos.Remove(boleto);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
